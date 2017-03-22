2bd4962e0a3e27683b0897c6963bb248d49bcde2
juliemdyer <juliemdyer@gmail.com>
2017-03-21 20:32:45 -0400

    Updated with Todd's edits

--- blackjack_OO.js
+++ blackjack_OO.js
@@ -1,172 +1,130 @@
-var deck;
 var dealerHand = [];
 var playerHand = [];
-var bets = new Bets();
 var currentHand;
+var deck;
+var deckCount;
+var countCards = true;
+var bets = new Bets();
 
-function Bets(){
-  this.pot = 500;
-  this.bet =0;
-  $('#bet').text(0);
-  $('#pot').text('$' + this.pot);
-
+// Bets Object
+function Bets() {
+    this.pot = 500;
+    this.bet = 0;
+    $('#bet').text(0);
+    $('#pot').text('$' + this.pot);
 }
 
-Bets.prototype.updateAmounts = function (){
-  $('#bet').text('$' + this.bet);
-  $('#pot').text('$' + this.pot);
+// Bets methods
+Bets.prototype.updateAmounts = function () {
+    $('#bet').text('$' + this.bet);
+    $('#pot').text('$' + this.pot);
 };
-
-Bets.prototype.potAmount = function(){
-  return this.pot;
-
-};
-
-Bets.prototype.enableDoubleDown = function () {
-  $('#doubledown').removeClass('disabled');
-
-};
-
-Bets.prototype.disableDoubleDown = function() {
-  $('#doubledown').removeClass('disabled');
-};
-
 Bets.prototype.doubleDown = function() {
-  this.pot -= this.bet;
-  this.bet += this.bet;
+    this.pot -= this.bet;
+    this.bet += this.bet;
 };
-
+Bets.prototype.potAmount = function() {
+    return this.pot;
+};
 Bets.prototype.betAmount = function(){
-  return this.bet;
-
+    return this.bet;
 };
 Bets.prototype.disableDeal = function () {
-  $('#deal-button').addClass('disabled');
-
+    $('#deal-button').addClass('disabled');
 };
-
-Bets.prototype.addBet = function(amount){
-
+Bets.prototype.addBet = function(amount) {
     if (this.pot >= amount) {
-      this.pot = this.pot - amount;
-      this.bet = this.bet + amount;
-      this.updateAmounts();
-      $('#deal-button').removeClass('disabled');
+        this.pot = this.pot - amount;
+        this.bet = this.bet + amount;
+        this.updateAmounts();
+        $('#deal-button').removeClass('disabled');
     } else {
-      notEnoughChips();
+        notEnoughChips();
     }
 };
-
-
-Bets.prototype.winner = function(){
-    this.pot += this.bet*2;
+Bets.prototype.winner = function() {
+    this.pot += this.bet * 2;
+    this.bet = 0;
+    this.updateAmounts();
+    this.disableDeal();
+};
+Bets.prototype.loser = function() {
+    this.bet = 0;
+    this.updateAmounts();
+    this.disableDeal();
+};
+Bets.prototype.push = function() {
+    this.pot += this.bet;
+    this.bet = 0;
+    this.updateAmounts();
+    this.disableDeal();
+};
+Bets.prototype.blackJackWinner = function() {
+    this.pot += parseInt(this.bet * 2.5);
     this.bet = 0;
     this.updateAmounts();
     this.disableDeal();
 };
 
-Bets.prototype.loser = function (){
-  this.bet = 0;
-  this.updateAmounts();
-  this.disableDeal();
-};
-
-Bets.prototype.push = function (){
-  this.pot += this.bet;
-  this.bet = 0;
-  this.updateAmounts();
-  this.disableDeal();
-};
-
-Bets.prototype.blackJackWinner = function(){
-    this.pot += parseInt(this.bet*2.5);
-    this.bet =0;
-    this.updateAmounts();
-    this.disableDeal();
-};
-
-Bets.prototype.updateOptions = function(){
-  if (playerHand.numCards()===2){
-    $('#doubledown').removeClass('disabled');
-  } else {
-    $('#doubledown').removeClass('disabled');
-  }
-
-};
 
 
-//              Card Ojbect
+// Card Object
+function Card(value,suit) {
+    this.point = value;
+    this.suit = suit;
+}
 
-function Card(value,suit){this.point = value; this.suit = suit;}
-
+// Card methods
 Card.prototype.getImageUrl = function(){
-  var sundry = this.point;
+    var sundry = this.point;
 
-  if (this.point === 1)  {sundry = 'ace';}
-  if (this.point === 11) {sundry= 'jack';}
-  if (this.point === 12) {sundry = 'queen';}
-  if (this.point === 13) {sundry = 'king';}
+    if (this.point === 1)  {sundry = 'ace';}
+    if (this.point === 11) {sundry= 'jack';}
+    if (this.point === 12) {sundry = 'queen';}
+    if (this.point === 13) {sundry = 'king';}
 
-  // return 'images/' + sundry + "_" + this.suit + ".png";
-  return '<img src="deck/' + sundry + '_' + this.suit + '.png">';
+    return '<img src="deck/' + sundry + '_' + this.suit + '.png">';
 };
-//      Hands Object
-function Hands() {
-  this.hands = [];
 
-}
 
-Hands.prototype.addHand = function(hand) {
-  this.hands.push(hand);
 
-};
-//            Hand Object
-
+// Hand Object
 function Hand() {
-  this.hand =[];
-  this.doubledown = false;
+    this.hand =[];
 }
 
+// Hand Methods
 Hand.prototype.addCard = function(card){
-  this.hand.push(card);
-
+    this.hand.push(card);
 };
-
-Hand.prototype.secondCardAce = function(){
-  return (this.hand[1].point === 1);
-
-};
-
 Hand.prototype.hasBlackJack = function(){
-  return (this.hand.length === 2 && this.getPoints() === 21);
+    return (this.hand.length === 2 && this.getPoints() === 21);
 };
-
-
 Hand.prototype.numCards = function(){
-  return this.hand.length;
-
+    return this.hand.length;
 };
-
 Hand.prototype.firstCard = function(){
-  return this.hand[0];
-
+    return this.hand[0];
 };
-
-Hand.prototype.doubleDown = function(){
-  this.doubleDown = true;
-};
-
 Hand.prototype.getPoints = function(){
-  var sumCards;
-  var sortedHand = this.hand.slice(0).sort(function(a,b){return b.point - a.point;});
-  sumCards = sortedHand.reduce(function(currentSum, card) {
+    var sumCards;
+    var ace;
+    var sortedHand = this.hand.slice(0).sort(function(a,b){
+        return b.point - a.point;});
+    var numberOfAces = this.hand.reduce(function(accum, card){
+    if (card.point === 1 ){ ace = 1; } else {ace = 0;}
+        return accum + ace;
+    },0);
+
+    sumCards = sortedHand.reduce(function(currentSum, card) {
       var tempCardPoint = card.point;
       // If face card - point value is 10
       if (card.point > 10) {
           tempCardPoint = 10;
       }
       if (card.point === 1){
-        if (currentSum + 11 > 21){
+        numberOfAces -=1;
+        if (currentSum + 11 + numberOfAces > 21){
           tempCardPoint = 1;
         } else {
           tempCardPoint = 11;
@@ -176,28 +134,43 @@ Hand.prototype.getPoints = function(){
   }, 0);
   return sumCards;
 };
-//                Deck Object
+
+
+
+// Deck Object
 function Deck() {
   this.deck = [];
+  deckCount = 0;
+  if (countCards){console.log("Shuffling the deck");}
   // Loop over each point value
   for (var points = 1;  points < 14; points++) {
       var suits =['spades','hearts','clubs','diamonds'];
       // Loop over each suit
       for (var suit in suits) {
           // Add each suit as an object to deck array
-
           this.deck.push(new Card(points,suits[suit]));
       } // End suit for loop
   } // End point for loop
 }
 
+// Deck methods
 Deck.prototype.draw = function(target) {
   var cardObject;
-
+  var strHint='';
   // Get random number from 1 to length of current deck
   var randomIndex = parseInt(Math.random() * (this.deck.length));
   // Get card object from deck at random index from line above
   cardObject = this.deck[randomIndex];
+  if (cardObject.point >= 2 && cardObject.point <= 6) {
+    deckCount +=1;
+
+  } else if (cardObject.point === 1 || cardObject.point >9){
+    deckCount -=1;
+  }
+  if (deckCount < -2){strHint = " (mostly low cards)";}
+  else if (deckCount > 2){strHint = " (mostly high cards)";}
+
+  if (countCards){console.log("Deck count: " + deckCount + strHint);}
 
   if (target === 'player') {
       playerHand.addCard(cardObject);
@@ -216,12 +189,11 @@ Deck.prototype.draw = function(target) {
 
   return cardObject;
 };
-
-
 Deck.prototype.numCardsLeft = function() {
   return this.deck.length;
-
 };
+
+
 
 // Initial deal, deals 2 cards to player, 2 to dealer
 function deal() {
@@ -258,7 +230,7 @@ function deal() {
     $('#deal-button').addClass('disabled');
     $('#hit-button').removeClass('disabled');
     $('#stand-button').removeClass('disabled');
-
+    $('.chips div').addClass('disabled');
     if (bets.potAmount() >= bets.betAmount()){
       $('#doubledown').removeClass('disabled');
     }
@@ -267,12 +239,39 @@ function deal() {
       dealerTurn();
     }
 
-    if(dealerHand.secondCardAce()){
-      $('#insurance').removeClass('disabled');
-    }
-
 }
 
+
+// Double down - deal card to player, update score and start Dealer's turn
+function doubleDown(){
+    deck.draw('player');
+    bets.doubleDown();
+    updatePlayerScore();
+    dealerTurn();
+}
+
+
+
+// Player portion, deal card to player and calculate points after that hit
+function hit() {
+    // After player hits, they are no longer able to double down
+    $('#doubledown').addClass('disabled');
+
+    //  Deal a card as player, update score
+    deck.draw('player');
+    updatePlayerScore();
+
+    // Player busts
+    if (playerHand.getPoints() >= 21) {
+        $('#hit-button').addClass('disabled');
+        $('#stand-button').addClass('disabled');
+        dealerTurn();
+    }
+}
+
+
+
+// Flip Hole card function
 function flipHoleCard() {
     var holeCard = dealerHand.firstCard();
     var sundry = holeCard.point;
@@ -282,34 +281,10 @@ function flipHoleCard() {
     if (holeCard.point === 13) {sundry = 'king';}
 
     var holeCardSrc = 'deck/' + sundry + '_' + holeCard.suit + '.png';
-
     $('#dealer-hand :first-child').attr('src', holeCardSrc);
 }
 
-function doubleDown(){
-  playerHand.doubleDown();
-  deck.draw('player');
-  bets.doubleDown();
-  updatePlayerScore();
-  dealerTurn();
-}
 
-// Player portion, deal card to player and calculate points after that hit
-function hit() {
-    $('#doubledown').addClass('disabled');
-    //  Deal a card as player
-
-    deck.draw('player');
-    updatePlayerScore();
-
-    if (playerHand.getPoints() >= 21) {
-        $('#hit-button').addClass('disabled');
-        $('#stand-button').addClass('disabled');
-        // $('#deal-button').removeClass('disabled');
-        dealerTurn();
-    }
-
-}
 
 // Start Dealer portion and check winner scenarios
 function dealerTurn() {
@@ -321,7 +296,7 @@ function dealerTurn() {
     var dealerHasBlackJack = dealerHand.hasBlackJack();
 
     $('#doubledown').addClass('disabled');
-    // if (playerPoints > 21 then game is over) {
+    // if playerPoints > 21 then game is over
     if (playerPoints > 21) {
         revealHoleCard = false;
         gameOver = true;
@@ -352,7 +327,6 @@ function dealerTurn() {
     if (gameOver === false){
         // At this point dealer takes cards until dealers has at least 17 points
         while (dealerPoints < 17){
-          // dealCard('dealer');
           deck.draw('dealer');
           dealerPoints = dealerHand.getPoints();
         }
@@ -366,16 +340,16 @@ function dealerTurn() {
         } else if (dealerPoints === playerPoints) {
           $('#messages').html('<h2>PUSH</h2>');
           bets.push();
-        } else {
+        } else if (dealerPoints > playerPoints){
           $('#messages').html('<h2>DEALER WINS</h2>');
           bets.loser();
         }
     }
 
-    // Make deal button only active button
-    // $('#deal-button').removeClass('disabled');
+    // Disable all buttons until player bets again
     $('#hit-button').addClass('disabled');
     $('#stand-button').addClass('disabled');
+    $('.chips div').removeClass('disabled');
 
     // If Player went bust then don't reveal hole card
     if (revealHoleCard === true){
@@ -386,19 +360,21 @@ function dealerTurn() {
     }
 }
 
-// Changes score display
+
+
+// Change score display
 function updatePlayerScore() {
-    // var playerPoints = calculatePoints(playerHand);
     var playerPoints = playerHand.getPoints();
     $('#player-label').text('PLAYER: ' + playerPoints);
 }
-
 function updateDealerScore() {
-    // var dealerPoints = calculatePoints(dealerHand);
     var dealerPoints = dealerHand.getPoints();
     $('#dealer-label').text('DEALER: ' + dealerPoints);
 }
 
+
+
+// Sweet Alert Pop Up Functions
 function outOfChips(){
     swal({
       title: "You're Out of Chips!",
@@ -408,10 +384,11 @@ function outOfChips(){
       function(isConfirm){
         if (isConfirm) {
           bets = new Bets();
+          $('#player-hand').children().remove();
+          $('#dealer-hand').children().remove();
         }
     });
 }
-
 function notEnoughChips(){
     swal({
       title: "Insufficient Chips!",
@@ -419,7 +396,6 @@ function notEnoughChips(){
       imageUrl: "img/chip-2.png"
     });
 }
-
 function welcome() {
   swal({
     title: "Welcome to Blackjack!",
@@ -428,22 +404,23 @@ function welcome() {
   });
 }
 
+
+
+// Document Ready
 $(function () {
 
     // Button click event handlers
-    $('#deal-button').click(deal);
-    $('#deal-button').addClass('disabled');
+    $('#deal-button').click(deal).addClass('disabled');
+    $('#doubledown').click(doubleDown);
     $('#hit-button').click(hit);
-
     $('#stand-button').click(dealerTurn);
 
+    // Bet click event handlers
     $('#five').click(function() {bets.addBet(5);});
     $('#ten').click(function() {bets.addBet(10);});
     $('#fifteen').click(function() {bets.addBet(15);});
     $('#fifty').click(function() {bets.addBet(50);});
-    $('#doubledown').click(doubleDown);
+
     welcome();
-
-
 
 }); // End DOM Ready
